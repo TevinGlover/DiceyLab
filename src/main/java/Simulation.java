9a3c52900a7f70fed3894fb33e8e7236cc2a9ede
Tevin Glover <tevwork.tg@gmail.com>
2023-11-10 16:11:11 -0500

    .. wewe

--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,4 +1,31 @@
 public class Simulation {
+// create constroctor  ( int number of dies, int numberofTossess)
+
+    int numberOfDices;
+    int numberOfTosses;
+
+   // create runSumlation Method
+
+    public Simulation(int numberOfDices, int numberOfTosses){
+
+        this.numberOfDices= numberOfDices;
+        this.numberOfTosses = numberOfTosses;
+    }
+    // use values in constructor
+    // to simulate the toss throwing
+
+
+
+
+// create a run simulation
+public void runSimulation(){
+    Dice dice = new Dice( this.numberOfDices);
+    Bins bins = new Bins(this.numberOfDices,this.numberOfDices * 6); // minimal number of dice to max.
+
+    // forloop tossses for any amount of throws
+    for ( int i= 0; i < this.numberOfTosses; i++){
+        bins.incrementBin(dice.tossAndSum()); // increment the value by the tosses method.
+
 
 
 
---
+++ src/test/java/SimulationTest.java
@@ -0,0 +1,17 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimulationTest {
+
+
+    @Test
+    public void testSmulationConsturtor(){
+        int numberOfDice =2;
+        int numberOfTosses = 10;
+        Simulation simulation = new Simulation(numberOfDice, numberOfTosses);
+        Assert.assertNotNull(simulation);
+    }
+
+}
